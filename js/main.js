1e9e954af7ba176a120c8cd7995a1e5749240201
56quique <enriqueklenser241@gmail.com>
2026-03-27 13:54:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+import { crearGauge } from "./gauge.js"
 
+const g1 = crearGauge("gaugeL1")
+const g2 = crearGauge("gaugeL2")
+const g3 = crearGauge("gaugeL3")
+
+function render() {
+  g1.dibujar()
+  g2.dibujar()
+  g3.dibujar()
+}
+
+render()
