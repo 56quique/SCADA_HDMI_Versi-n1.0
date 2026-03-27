608cef51bad5c8033cf6a2eefe43b8ff97d2186d
56quique <enriqueklenser241@gmail.com>
2026-03-27 13:22:28 -0300

    Update gauges.js

--- js/gauges.js
+++ js/gauges.js
@@ -1,42 +1,52 @@
-.leds {
-  margin-top: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-}
+export function crearGauge(canvasId) {
+  const canvas = document.getElementById(canvasId)
+  const ctx = canvas.getContext("2d")
 
-.led {
-  font-size: 10px;
-  padding: 2px 6px;
-  border-radius: 4px;
-  background: #333;
-  color: #aaa;
-  text-align: center;
-}
+  canvas.width = 120
+  canvas.height = 300
 
-/* ACTIVOS */
-.led.activo.rojo {
-  background: red;
-  color: white;
-}
+  function dibujar(valor, umin, umax) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-.led.activo.verde {
-  background: #22c55e;
-  color: black;
-}
+    const h = canvas.height
+    const w = canvas.width
 
-.led.activo.amarillo {
-  background: yellow;
-  color: black;
-}
-.controles {
-  margin-top: 10px;
-  font-size: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-}
+    // Fondo escala
+    ctx.fillStyle = "#0a0a0a"
+    ctx.fillRect(40, 10, 40, h - 20)
 
-.controles input {
-  width: 100%;
+    // Escala
+    for (let v = 0; v <= 300; v += 10) {
+      const y = h - (v / 300) * h
+
+      ctx.beginPath()
+      ctx.moveTo(30, y)
+      ctx.lineTo(v % 30 === 0 ? 80 : 70, y)
+      ctx.strokeStyle = "#aaa"
+      ctx.stroke()
+    }
+
+    // Líneas Umin / Umax
+    const yMin = h - (umin / 300) * h
+    const yMax = h - (umax / 300) * h
+
+    ctx.strokeStyle = "yellow"
+    ctx.beginPath()
+    ctx.moveTo(20, yMin)
+    ctx.lineTo(100, yMin)
+    ctx.stroke()
+
+    ctx.strokeStyle = "red"
+    ctx.beginPath()
+    ctx.moveTo(20, yMax)
+    ctx.lineTo(100, yMax)
+    ctx.stroke()
+
+    // Cursor
+    const yVal = h - (valor / 300) * h
+    ctx.fillStyle = "#00ffff"
+    ctx.fillRect(35, yVal - 3, 50, 6)
+  }
+
+  return { dibujar }
 }
