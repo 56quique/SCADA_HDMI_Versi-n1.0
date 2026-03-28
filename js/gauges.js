d00c49dc8cf4e49de257cae7ade93eaf1b8a1bd9
56quique <enriqueklenser241@gmail.com>
2026-03-28 13:53:00 -0300

    Actualizar gauges.js

--- js/gauges.js
+++ js/gauges.js
@@ -11,7 +11,7 @@ export function crearGauge(id, min, max, unidad = "") {
 
   const ctx = canvas.getContext("2d")
 
-  const WIDTH = 90
+  const WIDTH = 80
 const HEIGHT = 140
 
   canvas.width = WIDTH
