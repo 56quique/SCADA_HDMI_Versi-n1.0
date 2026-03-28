43c3140b506daea6d71767e6b1cc5df455d6d57c
56quique <enriqueklenser241@gmail.com>
2026-03-28 07:47:58 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,11 +11,15 @@ import { crearGauge } from "./gauges.js"
 // CREACIÓN DE INSTRUMENTOS
 // =============================
 
-// Gauge de tensión RED
-const gaugeRed = crearGauge("gaugeV1", 0, 300, "V")
+// RED
+const redV1 = crearGauge("redV1", 0, 300, "V")
+const redV2 = crearGauge("redV2", 0, 300, "V")
+const redV3 = crearGauge("redV3", 0, 300, "V")
 
-// Gauge de tensión GRUPO
-const gaugeGrupo = crearGauge("gaugeV2", 0, 300, "V")
+// GRUPO
+const grupoV1 = crearGauge("grupoV1", 0, 300, "V")
+const grupoV2 = crearGauge("grupoV2", 0, 300, "V")
+const grupoV3 = crearGauge("grupoV3", 0, 300, "V")
 
 // =============================
 // LOOP PRINCIPAL (tipo SCADA)
@@ -32,8 +36,13 @@ function loop(timestamp) {
   }
 
   // dibujar SIEMPRE (suavizado hace el resto)
-  gaugeRed.dibujar(estado.red.tension[0])
-  gaugeGrupo.dibujar(estado.grupo.tension[0])
+ redV1.dibujar(estado.red.tension[0])
+redV2.dibujar(estado.red.tension[1])
+redV3.dibujar(estado.red.tension[2])
+
+grupoV1.dibujar(estado.grupo.tension[0])
+grupoV2.dibujar(estado.grupo.tension[1])
+grupoV3.dibujar(estado.grupo.tension[2])
 
   requestAnimationFrame(loop)
 }
