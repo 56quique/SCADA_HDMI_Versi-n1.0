b5039aa6dc2563797d2bb15b6b9eafbf00e8f3e9
56quique <enriqueklenser241@gmail.com>
2026-03-28 06:40:48 -0300

    Add electrical system state object for SCADA

--- js/data.js
+++ js/data.js
@@ -1,1 +1,32 @@
+// Este objeto representa TODO el sistema eléctrico
+// Es la base del SCADA
 
+export const estado = {
+
+  // Datos de la RED
+  red: {
+    tension: [220, 220, 220],   // L1, L2, L3
+    corriente: [0, 0, 0],       // L1, L2, L3
+    frecuencia: 50,
+    potencia: {
+      p: 0,     // activa
+      q: 0,     // reactiva
+      s: 0,     // aparente
+      cosfi: 1
+    }
+  },
+
+  // Datos del GRUPO ELECTRÓGENO
+  grupo: {
+    tension: [0, 0, 0],
+    corriente: [0, 0, 0],
+    frecuencia: 50,
+    potencia: {
+      p: 0,
+      q: 0,
+      s: 0,
+      cosfi: 1
+    }
+  }
+
+}
